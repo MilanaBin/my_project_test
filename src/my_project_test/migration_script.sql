efbc3b9142a9338def28d29546724acd3e4d228e
MilanaBin <vgpv.milana@gmail.com>
2024-11-26 17:46:07 +0500

    update

--- src/my_project_test/migration_script.sql
+++ src/my_project_test/migration_script.sql
@@ -4,6 +4,12 @@ CREATE TABLE bookings.airports_data (
 	airport_name jsonb NOT NULL,
 	city jsonb NOT NULL,
 	coordinates point NOT NULL,
-	timezone varchar NOT NULL,
-    load_date date null
+	timezone varchar NOT NULL
 );
+
+COMMENT ON TABLE bookings.airports_data IS 'Данные об аэропортах, включая код, название, город, координаты и часовой пояс';
+COMMENT ON COLUMN bookings.airports_data.airport_code IS 'Код аэропорта (3 символа)';
+COMMENT ON COLUMN bookings.airports_data.airport_name IS 'Название аэропорта в формате JSONB, поддерживающее локализацию';
+COMMENT ON COLUMN bookings.airports_data.city IS 'Город в формате JSONB, поддерживающий локализацию';
+COMMENT ON COLUMN bookings.airports_data.coordinates IS 'Географические координаты аэропорта (точка: долгота и широта)';
+COMMENT ON COLUMN bookings.airports_data.timezone IS 'Часовой пояс аэропорта';
