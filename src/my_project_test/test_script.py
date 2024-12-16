5e7042473628e5b35a63b9c75f3eb8e409213ad5
MilanaBin <vgpv.milana@gmail.com>
2024-12-16 23:16:21 +0500

    add

--- src/my_project_test/test_script.py
+++ src/my_project_test/test_script.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
         "dbname": "postgres",
         "user": "postgres",
         "password": "11111",
-        "port": 5432,
+        "port": 5433,
     }
 
     table_name = "example_table"
