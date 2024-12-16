15368207de9ea60441ebfce3b9dd6a7283707f44
MilanaBin <vgpv.milana@gmail.com>
2024-12-16 17:54:09 +0500

    check

---
+++ src/my_project_test/test_connection.py
@@ -0,0 +1,22 @@
+import psycopg2
+import os
+
+
+def main():
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv("DB_HOST", "postgres"),
+            port=os.getenv("DB_PORT", "5432"),
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+        )
+        print("✅ Successfully connected to PostgreSQL")
+        conn.close()
+    except Exception as e:
+        print("❌ Connection failed:", e)
+        raise SystemExit(1)
+
+
+if __name__ == "__main__":
+    main()
